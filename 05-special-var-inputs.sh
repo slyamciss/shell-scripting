6e8b3cc616c8e9af4f2a45eaa593111c84e0f8fc
amciss <slyamciss@gmail.com>
2022-01-21 19:55:38 -0500

    roboshop project structure installation

--- 05-special-var-inputs.sh
+++ 05-special-var-inputs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo $0
-echo $1
-echo $2
-echo $#
-echo $*
-echo $@
+echo $0   #Script Name
+echo $1   #First Argument
+echo $2   #Second Argument
+echo $#   #Number of Argument
+echo $*   #All Arguments
+echo $@   #All Argument
