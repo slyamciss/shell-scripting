952b7447fa7f1bdf72081ec679199b5d5a1c5b99
amciss <slyamciss@gmail.com>
2022-01-23 10:06:20 -0500

    change structure

--- 03-var.sh
+++ 03-var.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 student_name='Tariq'
-echo student_name = $student_name
-echo student_name = ${student_name}
+echo Student Name = $student_name
+echo Student Name = ${student_name}
 
 DATE=2022-01-22
 echo Good afternoon, today is $DATE
