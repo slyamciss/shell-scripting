36be3bdb37d38b01cd9f43996f0dd46d81b9cab7
amciss <slyamciss@gmail.com>
2022-01-23 20:11:24 -0500

    steps added

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,5 +1,20 @@
 
+LOG_FILE=/tmp/roboshop.log
+rm -f $LOG_FILE
+
 echo "Installing NGINX"
-yum install nginx -y >>/tmp/roboshop.log
+yum install nginx -y &>>$LOG_FILE
+
 echo "Download Frontend Content"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>/tmp/roboshop.log
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
+
+echo  "Clean Old Content"
+rm -rf /usr/share/nginx/html/*  &>>$LOG_FILE
+
+echo "Extract Frontend Content"
+cd /tmp
+unzip /tmp/frontend.zip  &>>$LOG_FILE
+
+echo "Copy extracted Content to NGINX Path"
+cp -r frontend-main/static/*  /usr/share/nginx/html/  &>>$LOG_FILE
+
