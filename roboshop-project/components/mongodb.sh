571b1e39a0827655dcf0e121ba98abf6c1161757
amciss <slyamciss@gmail.com>
2022-01-24 15:12:35 -0500

    mongodb config

--- roboshop-project/components/mongodb.sh
+++ roboshop-project/components/mongodb.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo installing mongodb
+source components/common.sh
+
+echo "Download mongodb repo file"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
+
+echo "Install MongoDB"
+yum install -y mongodb-org &>>$LOG_FILE
