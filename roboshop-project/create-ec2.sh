76d09b801b0c1347a52e19adcde0daee1963f565
amciss <slyamciss@gmail.com>
2022-01-26 20:17:36 -0500

    create file updated

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -3,6 +3,11 @@
 LOG=/tmp/instance-create.log
 rm -f $LOG
 
+if [ "$1" == "list" ]; then
+  aws ec2 describe-instances  --query "Reservations[*].Instances[*].{PrivateIP:PrivateIpAddress,PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}"  --output table
+  exit
+fi
+
 INSTANCE_NAME=$1
 if [ -z "${INSTANCE_NAME}" ]; then
   echo -e "\e[1;33mInstance Name Argument is needed\e[0m"
@@ -21,11 +26,13 @@ fi
 PRIVATE_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 
 if [ -z "${PRIVATE_IP}" ]; then
+  # Find Security Group
   SG_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values=allow-all-ports --query "SecurityGroups[*].GroupId" --output text)
   if [ -z "${SG_ID}" ]; then
     echo -e "\e[1;33m Security Group allow-all-ports does not exist"
     exit
   fi
+  # Creating Instance
   aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"  --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids "${SG_ID}"  &>>$LOG
   echo -e "\e[1m Instance Created\e[0m"
 else
